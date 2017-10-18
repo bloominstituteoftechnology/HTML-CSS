e160c9eba7e1551f3f2d0a94b8be2656fdd94968
Alex Cassell <alex@simulism.net>
2017-10-18 15:02:21 -0400

    The html file, is supposed to be a php file, so that the form will work. However, to make it easier to run locally I have changed it to an html file and commneted out the contact form.website stills needs a background and is missing all js animation, that will tie it all up. Also, only the large @media is finished

---
+++ js/animationFrontpage.js
@@ -0,0 +1,17 @@
+//    Created by: Alex Cassell 10-17-2017
+//    AlexCassell.com
+//    Created using Visual Studio Code and Mountain Dew
+//    © Alex Cassell 2017 All rights Reserved.
+//    Provided without warranty;
+//    If this script breaks your computer
+//    or knocks up your sister (has only happened twice), 
+//    you should have expected this and are on your own.
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+/*jslint node: true */
+/*jshint strict:true */
+"use strict";
+
+function Init() {
+    $('#planetOne').animate({'left': '-40px'}, 1000);
+}
