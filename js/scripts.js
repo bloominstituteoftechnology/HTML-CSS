b94ed0b9eb4e838b6a8842412e5f47c7305dd463
codealtgeek <codealtgeek@gmail.com>
2017-10-19 15:55:22 -0400

    Finished

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//JavaScript/jQuery 
+$("button.script").click(function(){
+   $(".hidden-content-1").toggle();
+ });
+
+ $("button.script1").click(function(){
+  $(".hidden-content-2").toggle();
+});
+$("button.script3").click(function(){
+  $(".hidden-content-3").toggle();
+});
+$("button.script4").click(function(){
+  $(".hidden-content-4").toggle();
+});
