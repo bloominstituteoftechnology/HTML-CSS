53bdbebd1f925d582cc928895ed6deccc2d920aa
David Long <davdaarn@live.com>
2017-10-20 17:26:27 -0500

    timed commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,44 @@
+class CurrentState {
+  constructor(nav, content) {
+    this.nav = nav;
+    this.content = content;
+    this.initializeDefaultState();
+    console.log(this.nav);
+    console.log(this.content);
+  }
+
+  setCurrent(navClicked) {
+    document
+      .querySelector(`.content__page[data-page='${navClicked}']`)
+      .classList.remove('hidden');
+  }
+  removeLast() {
+    // document.querySelector(`.link__item[data-link='${navClicked}']`).classList.remove('hidden')
+  }
+  initializeDefaultState() {
+    for (let i = 1; i < this.nav.length; i++) {
+      content[i].classList.add('hidden');
+    }
+  }
+}
+
 class SelectedContent {
   constructor(nav, content) {
     this.nav = nav;
     this.content = content;
-    this.nav.addEventListener('mouseover', () => {
-      console.log(this.nav.innerText);
+    this.nav.addEventListener('click', () => {
+      // console.log(this.nav.innerText);
+      // console.log(this.content.innerText);
+      currentState.setCurrent(this.nav.dataset.link);
     });
   }
-
-
-
-
 }
 
 let nav = document.querySelectorAll('.link__item');
 let content = document.querySelectorAll('.content__page');
+let currentState = new CurrentState(nav, content);
 let selectedContent = Array.from(nav).map((cv, i) => {
   return new SelectedContent(nav[i], content[i]);
 });
 
-console.log(selectedContent);
+// console.log(currentState);
--- src/js/dummy.js
+++ src/js/dummy.js
@@ -19,3 +19,11 @@ class MyClass {
  * #282c34
  * #98c379
  */
+
+
+for (var i = 1; i <= 10; i++) {
+  setTimeout(function(i) {
+      // From looking at the code you would assume it would print 1 - 10
+      // It doesn't.  Why?  How can you make it print 1 - 10.
+      console.log(i);
+  }, 0,i)
