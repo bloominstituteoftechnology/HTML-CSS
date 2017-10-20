aa17912ed1460a31a157db9dfef4e4ffa931e90b
Perry Ahern <pahern@gmail.com>
2017-10-20 02:02:06 -0400

    end of night, save!

--- scripts.js
+++ scripts.js
@@ -1,57 +1,34 @@
-let initProjects = showNumProjects => {
-  console.log('Initialized!');
-  for (let i = 0; i < showNumProjects; i++) {
-    allProjects[i].classList.remove('hide');
-  }
-};
-
-let shiftRight = showNumProjects => {
-  let allProjects = document.getElementsByClassName('projects--each');
-  // console.log(allProjects);
-  for (let i = 0; i < showNumProjects; i++) {
-    // remove each visible project as encountered
-    if (!allProjects[i].classList.contains('hide')) allProjects[i].classList.add('hide');
-    // find the corrected project number if it wraps past the right end
-    let projectNum = i + showNumProjects >= allProjects.length ? (i + showNumProjects) - showNumProjects.length : i + showNumProjects;
-    // console.log('sr1: ' + i, projectNum);
-    // display each hidden element (limited by showNumProjects loop)
-    allProjects[projectNum].classList.remove('hide');
-  }
-  // console.log(allProjects);
-};
-
-let shiftRight2 = showNumProjects => {
-  let allProjects = document.getElementsByClassName('projects--each');
-  // console.log(allProjects);
-  for (let i = 0; i < showNumProjects; i++) {
-    // remove each visible project as encountered
-    if (!allProjects[i].classList.contains('hide')) allProjects[i].classList.add('hide');
-    // find the corrected project number if it wraps past the right end
-    let projectNum = i + showNumProjects >= allProjects.length ? (i + showNumProjects) - showNumProjects.length : i + showNumProjects;
-    // console.log('sr2: ' + i, projectNum);
-    // display each hidden element (limited by showNumProjects loop)
-    allProjects[projectNum].classList.remove('hide');
-  }
-  // console.log(allProjects);
-};
-
-// let shiftLeft = showNumProjects => {
+// let initProjects = showNumProjects => {
+//   console.log('Initialized!');
 //   for (let i = 0; i < showNumProjects; i++) {
-//     allProjects[i + showNumProjects].classList.add('hide');
 //     allProjects[i].classList.remove('hide');
 //   }
 // };
 
-let allProjects = document.getElementsByClassName('projects--each');
+// let shiftRight = showNumProjects => {
+//   for (let i = 0; i < showNumProjects; i++) {
+//     if (!allProjects[i].classList.contains('hide')) allProjects[i].classList.add('hide');
+//     let projectNum = i + showNumProjects >= allProjects.length ? (i + showNumProjects) - showNumProjects.length : i + showNumProjects;
+//     allProjects[projectNum].classList.remove('hide');
+//   }
+// };
+
+// let allProjects = document.querySelectorAll('.projects--each');
+// let numProjectsToShow = 3;
+
+// initProjects(numProjectsToShow);
 // console.log(allProjects);
-let numProjectsToShow = 3;
+// shiftRight(numProjectsToShow);
+// shiftRight(numProjectsToShow);
+// // console.log(allProjects);
 
-initProjects(numProjectsToShow);
-console.log(allProjects);
-shiftRight(numProjectsToShow);
-console.log(allProjects);
+class Projects {
+  constructor(element) {
+    this.element = element;
+  }
+}
 
-// shiftRight2(numProjectsToShow);
-// shiftLeft(numProjectsToShow);
+let projects = document.querySelectorAll('.projects');
+projects = Array.from(projects).map(projects => new Projects(projects));
 
-// let hiddenProjects = document.getElementsByClassName('hide');
+console.log(projects);
