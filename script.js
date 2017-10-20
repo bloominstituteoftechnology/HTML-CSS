d22d4fa6951d3c1bd614ede6a1d0cff34a28c36e
Austin F <austindanielfrench@gmail.com>
2017-10-20 15:44:45 -0700

    progress and restructuring the layout of the webpage

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function openNav() {
+    document.querySelector(".side-nav").style.width = "250px";
+    document.querySelector("body").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
 
+function closeNav() {
+    document.querySelector(".side-nav").style.width = "0";
+    document.querySelector("body").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
