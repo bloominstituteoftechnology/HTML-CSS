818072e934d3f4c10afe26679e8ba2ef16673316
Perry Ahern <pahern@gmail.com>
2017-10-22 11:21:35 -0400

    progress on shift functions, edge cases to resolve

--- components/mainContent/projectSection/projects.js
+++ components/mainContent/projectSection/projects.js
@@ -30,16 +30,17 @@ let getIndexesOfVisibleProjects = (projects, numProjects, numToShow) => {
   return visibleProjects;
 };
 
-let shiftRight = (projects, numProjects, numToShow) => {                      // Shift project display right sNP projects!
+let shiftRight = (projects, numProjects, numToShow) => {             // Shift project display right nTS projects!
   const visibleProjectsList = getIndexesOfVisibleProjects(projects, numProjects, numToShow);
   const numVisibleProjects = visibleProjectsList.length;
 
-  if (numProjects <= numToShow) return;             // If all projects are displayed, exit
+  if (numProjects <= numToShow) return;                              // If all projects are displayed, exit
 
   for (let i = 0; i < numVisibleProjects; i++) {
-    projects[visibleProjectsList[i]].classList.add('hide');
-    if (visibleProjectsList[i] + numToShow <= numProjects) {
-      if (visibleProjectsList[i] + numToShow >= numProjects) break;
+    projects[visibleProjectsList[i]].classList.add('hide');// ERROR: in case i === 0 & vPL[i] === 5, hides 5 (correct)
+    if (visibleProjectsList[i] + numToShow <= numProjects) {   // ERROR continued: then passes this
+      if (visibleProjectsList[i] + numToShow >= numProjects) break;// ERROR continued: then passes this and breaks loop
+               // ERROR continued: incorrect display result: 5 was hidden, none displayed, so 6, 7 remain visible
       projects[visibleProjectsList[i] + numToShow].classList.remove('hide');
     } else {
       projects[i].classList.remove('hide');
@@ -52,99 +53,39 @@ let shiftRight = (projects, numProjects, numToShow) => {                      //
   }
 };
 
-let shiftLeft = (projects, numProjects, numToShow) => {                      // Shift project display right sNP projects!
+let shiftLeft = (projects, numProjects, numToShow) => {              // Shift project display left nTS projects!
   const visibleProjectsList = getIndexesOfVisibleProjects(projects, numProjects, numToShow);
   const numVisibleProjects = visibleProjectsList.length;
 
-  if (numProjects <= numToShow) return;             // If all projects are displayed, exit
+  if (numProjects <= numToShow) return;                              // If all projects are displayed, exit
 
   for (let i = 0; i < numVisibleProjects; i++) {
     projects[visibleProjectsList[i]].classList.add('hide');
-    if (visibleProjectsList[i] - numToShow >= 0) {                               //  -  =  0
-      // projects[numProjects - (numToShow - i)].classList.remove('hide');     //  -
+    if (visibleProjectsList[i] - numToShow >= 0) {
       projects[visibleProjectsList[i] - numToShow].classList.remove('hide');
     } else if (i === 0 && visibleProjectsList[i] === numToShow - 1) {
-      // if (visibleProjectsList[i] - numToShow < 0) break;
-      console.log('else if');
+      // do nothing, allows for less than numToShow to display without filling gap
     } else {
-      // projects[visibleProjectsList[i] - numToShow].classList.remove('hide');
       projects[numProjects - (numToShow - i)].classList.remove('hide');
     }
   }
   if (numVisibleProjects < numToShow) {
-    // console.log('need extra');
+    console.log('need extra');
     for (let i = numVisibleProjects; i < numToShow; i++) {
-      // console.log(i, projects[numProjects - i - 1].classList.contains('hide'));
-      // if (i === 2 /* && projects[numProjects - i - 1] !== 7 */) {          // not working right on rrl or lr
-        // console.log('hit it');
-        // projects[numProjects - 1].classList.remove('hide');
-        // break;
-      // } else {
+      console.log('i:', i, '/ hidden?', projects[numProjects - i - 1].classList.contains('hide'));
+      if (i === 2 /* && projects[numProjects - i - 1] !== 7 */) {          // not working right on rrl or lr
+        console.log('hit it');
+        projects[numProjects - 1].classList.remove('hide');
+        break;
+      } else {
         projects[numProjects - i - 1].classList.remove('hide');
         console.log('missed it');
-      // }
+      }
     }  
   }
 };
 
-  // for (let i = 0; i < projectCount; i++) {                 // Loop to adjust visible projects
-  //   if (!allProjects[i].classList.contains('hide')) {      // If the current project is not hidden (it's visible)
-  //     if (projectCount - i > showNumProjects) {            // If displayed projects do not include last project in list
-  //       for (let j = i; j < i + showNumProjects; j++) {    // Start at i and loop over sNP displayed items
-  //         allProjects[j].classList.add('hide');            // Hide project j
-  //         if (j + showNumProjects < projectCount) {        // If next to display is before list end
-  //           allProjects[j + showNumProjects].classList.remove('hide');  // Show project j + sNP
-  //         }
-  //       }
-  //       break;                                             // All done, exit
-  //     } else {                                             // Display includes last project in list
-  //       let showMissing = 0;                               // Track # shown to determine # missing from last display
-  //       for (let j = 0; j < projectCount - i; j++) {       // loop through visible projects
-  //         allProjects[i + j].classList.add('hide');        // Hide project i + j
-  //         allProjects[j].classList.remove('hide');         // Show project j
-  //         showMissing++;                                   // Increment number of projects showing for calculation later
-  //       }
-  //       for (let j = showMissing; j < showNumProjects; j++) {   // Loop over # of projects missing from last display
-  //         allProjects[j].classList.remove('hide');              // Display next hidden project
-  //       }
-  //     }
-  //   }
-  // }
-// };
-
-// let shiftLeft = showNumProjects => {                       // Shift project display left sNP projects!
-//   let projectCount = allProjects.length;                   // Replace duplicate length calculations
-  
-//   if (projectCount <= showNumProjects) return;             // If all projects are displayed, exit
-
-//   for (let i = 0; i < projectCount; i++) {                 // Loop to adjust visible projects
-//     if (!allProjects[i].classList.contains('hide')) {      // If the current project is not hidden (it's visible)
-//       if (i < showNumProjects) {                       //***       // If displayed projects includes first project in list
-//         let hideExtra = 0;               //counter for later
-//         for (let j = i; j < i + showNumProjects; j++) {    // Start at i and loop over sNP displayed items
-//           allProjects[j].classList.add('hide');            // Hide project j
-//           if (showNumProjects - j >= 0) {          //***         // If next to display is at or after list start
-//             allProjects[projectCount - (3 - j)].classList.remove('hide'); //*** // Show project j + sNP
-//           }
-//         }
-//         break;                                             // All done, exit
-//       } else {                                             // Display did not include last project in list
-//         let showMissing = 0;                               // Track # shown to determine # missing from last display
-//         for (let j = i; j < i + showNumProjects; j++) {       // loop through visible projects
-//           allProjects[j].classList.add('hide');        // Hide project i + j
-//           allProjects[j - showNumProjects].classList.remove('hide');         // Show project j
-//           showMissing++;                                   // Increment number of projects showing for calculation later
-//         }
-//         if (showMissing === showNumProjects) break;        // All done, exit
-//         for (let j = showMissing; j < showNumProjects; j++) {   // Loop over # of projects missing from last display
-//           allProjects[projectCount - j + 1].classList.remove('hide');              // Display next hidden project
-//         }
-//       }
-//     }
-//   }
-// };
-
-// Needed: code shift left/right buttons to only display if # of projects > numProjectsToShow
+// Needed: code shift left/right buttons to only display if projectCount > numProjectsToShow
 
 const allProjects = document.querySelectorAll('.projects--each');
 const projectCount = allProjects.length;
