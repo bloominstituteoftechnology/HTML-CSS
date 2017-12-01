2530fe1a22a9ee689f3567e14a6c7715bf5f6bfa
FJF616 <franzjfernando@hotmail.com>
2017-12-01 04:56:59 -0800

    fixed tab select and deselect colors

--- Js/tabs.js
+++ Js/tabs.js
@@ -1,15 +1,16 @@
 //create tabs that display info when clicked.
 function openTab(event, tabName) {
     //correctly select the correct tab and deselct where appropriate.
-    var i, tabData, tabLinks;
+    let i, tabData, tabLinks;
     tabContent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabContent.length; i++) {
+    for (let i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = "none";
     }
     tabLinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tabLinks.length; i++) {
-        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    for (let i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace("active", "");
+    }
     document.getElementById(tabName).style.display = "block";
     event.currentTarget.className += " active";
-    }
+    
 }
