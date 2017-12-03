d0a01635c84cb32cc37ab50d6203fc56d72b9a21
Aaron Wilder <element1028@gmail.com>
2017-12-03 09:11:17 -0800

    My imagination is better then my CSS Fu

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const menuBtn = document.querySelector("#menu-btn");
-menuBtn.addEventListener("click", showMenu, false);
+menuBtn.addEventListener("click", showMenu, true);
 
 const flyoutMenu = document.querySelector("#flyoutMenu");
-flyoutMenu.addEventListener("click", hideMenu, false);
+flyoutMenu.addEventListener("click", hideMenu, true);
 
+const mouse = document.querySelector('.Mouse');
 function showMenu(e) {
+    menuBtn.classList.toggle("change")
     flyoutMenu.classList.add("show");
+    mouse.hidden = true;
     document.body.style.overflow = "hidden";
 }
 
 function hideMenu(e) {
+    menuBtn.classList.remove("change")
     flyoutMenu.classList.remove("show");
+    mouse.hidden = false;
     e.stopPropagation()
-
     document.body.style.overflow = "auto";
 }
 window.addEventListener(
